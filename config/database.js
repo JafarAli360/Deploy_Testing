498ff6fbce9997af6b9329e1d56036d25e403a7d
JafarAli360 <ahmad.19058@mhs.unesa.ac.id>
2022-11-17 11:11:30 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,40 @@
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_HOST,
+  DB_PORT,
+  DATABASE_URL,
+  PGDATABASE,
+  PGHOST,
+  PGPASSWORD,
+  PGPORT,
+  PGUSER,
+} = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_development`,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres",
+  },
+  test: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_development`,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "postgres",
+  },
+  production: {
+    username: PGUSER,
+    password: PGPASSWORD,
+    database: PGDATABASE,
+    host: PGHOST,
+    port: PGPORT,
+    dialect: "postgres",
+  },
+};
